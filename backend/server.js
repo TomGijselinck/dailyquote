9751c28f9e82b40ed7d6facde60991281c3e8d9c
Tom Gijselinck <tomgijselinck@gmail.com>
2016-05-21 14:57:00 +0200

    Add cross origin headers

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,11 @@ MongoClient.connect(url, (err, database) => {
   }
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
